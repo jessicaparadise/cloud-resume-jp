7ccf59ee95d1a65ea8acd610f184b73a99115969
Jessica Paradise <jessica.paradise@protonmail.com>
2025-10-30 17:29:57 -0700

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,15 +1,3 @@
-
-resource "aws_route53_record" "root_domain" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = var.domain
-  type    = "A"
-  alias {
-    name                   = aws_cloudfront_distribution.website.domain_name
-    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
 terraform {
   required_version = ">= 1.6.0"
 
