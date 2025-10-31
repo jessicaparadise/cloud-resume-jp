47ebf894f5ea301547b369871f669c672fab65b0
Jessica Paradise <jessica.paradise@protonmail.com>
2025-10-30 17:02:08 -0700

    Add Route 53 record resource for root domain
    Add Route 53 record for root domain with CloudFront alias

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -3,3 +3,15 @@ variable "domain" {
   type        = string
   default     = "jessicaparadise.com"
 }
+
+resource "aws_route53_record" "root_domain" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.website.domain_name
+    zone_id                = aws_cloudfront_distribution.website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
